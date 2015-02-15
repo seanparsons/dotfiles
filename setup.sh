49619938ae5945873dafc4c3886f508ac9b374d1
Sean Parsons <github@futurenotfound.com>
2015-02-15 21:02:21 +0000

    Added Docker stuff.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:webupd8team/sublime-text-3
-add-apt-repository -y ppa:oibaf/graphics-drivers
 add-apt-repository -y ppa:gnome3-team/gnome3
 add-apt-repository -y ppa:gnome3-team/gnome3-staging
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb https://get.docker.io/ubuntu docker main /etc/apt/sources.list.d/docker.list"
 apt-get update
 apt-get dist-upgrade
-apt-get install -y git haskell-platform-prof vpnc gdebi-core oracle-java8-installer sublime-text-installer xclip ppa-purge zsh curl
+apt-get install -y git haskell-platform-prof vpnc gdebi-core oracle-java8-installer sublime-text-installer xclip ppa-purge zsh curl lxc-docker
 cd /tmp
 rm -f *.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
