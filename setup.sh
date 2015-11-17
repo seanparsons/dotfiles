4be374ee066b0985503bd7dc17bbc21a296aded1
Sean Parsons <seanparsons@users.noreply.github.com>
2015-11-17 17:31:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-apt-get install -y software-properties-common
+apt-get install -y software-properties-common apt-transport-https
 apt-get remove -y oneconf
-add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:webupd8team/sublime-text-3
-add-apt-repository -y ppa:hvr/ghc
 wget -q -O- http://download.fpcomplete.com/ubuntu/fpco.key | apt-key add -
 echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main'|sudo tee /etc/apt/sources.list.d/fpco.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
@@ -11,7 +9,7 @@ sh -c "echo deb https://get.docker.io/ubuntu docker main\
 > /etc/apt/sources.list.d/docker.list"
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y git vpnc gdebi-core oracle-java8-installer sublime-text-installer xclip ppa-purge zsh curl lxc-docker htop python-pip vlc ant ack-grep stack
+apt-get install -y git vpnc gdebi-core openjdk-8-jdk sublime-text-installer xclip ppa-purge zsh curl lxc-docker htop python-pip vlc ant ack-grep stack
 sudo -u sean mkdir /home/sean/bin
 pip install awscli
 cd /tmp
