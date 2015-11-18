e44e8567f96f70f60180d4403e8e4f40feb47d65
Sean Parsons <seanparsons@users.noreply.github.com>
2015-11-18 11:29:14 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get install -y software-properties-common apt-transport-https
 apt-get remove -y oneconf
 add-apt-repository -y ppa:webupd8team/sublime-text-3
 wget -q -O- http://download.fpcomplete.com/ubuntu/fpco.key | apt-key add -
-echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main' | tee /etc/apt/sources.list.d/fpco.list
+echo 'deb http://download.fpcomplete.com/ubuntu/wily stable main' | tee /etc/apt/sources.list.d/fpco.list
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-wily main' | tee /etc/apt/sources.list.d/docker.list
 apt-get update
