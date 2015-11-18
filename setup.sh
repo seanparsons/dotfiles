d9280e1384e43d2cc28b496896be227f4478c99c
Sean Parsons <seanparsons@users.noreply.github.com>
2015-11-18 11:16:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,10 @@ sh -c "echo deb https://get.docker.io/ubuntu docker main\
 > /etc/apt/sources.list.d/docker.list"
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y git vpnc gdebi-core openjdk-8-jdk sublime-text-installer xclip ppa-purge zsh curl lxc-docker htop python-pip vlc ant ack-grep stack
+apt-get install -y git vpnc gdebi openjdk-8-jdk sublime-text-installer xclip ppa-purge zsh curl lxc-docker htop python-pip vlc ant ack-grep stack
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget -q http://downloads.typesafe.com/scala/2.11.5/scala-2.11.5.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 dpkg -i scala-2.11.5.deb
-sudo -u sean mkdir /home/sean/bin
 pip install awscli
-cd /tmp
 rm -f *.deb
-rm -rf /home/sean/.zshrc
-rm -rf /home/sean/.oh-my-zsh/
-curl -L http://install.ohmyz.sh | sh
-chown sean:sean /home/sean/.zshrc
-chown -R sean:sean /home/sean/.oh-my-zsh/
-chsh -s /bin/zsh sean
